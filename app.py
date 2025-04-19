a9afc9c897c7a57d784ee6afe677b812dea5172f
Gustavo <gustavo.estudo017@gmail.com>
2025-04-19 16:36:02 -0300

    deploy

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from flask_compress import Compress
-from cachetools import TTLCache
-import requests
-import heapq
-import os
 
 app = Flask(__name__)
 
