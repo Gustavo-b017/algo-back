f5194eda9865a74dbcff33cadc92af5592085ded
Gustavo <gustavo.estudo017@gmail.com>
2025-10-15 17:43:34 -0300

    ff

--- routes/auth.py
+++ routes/auth.py
@@ -7,6 +7,7 @@ from decorators.auth_decorator import login_required
 
 auth_bp = Blueprint("auth", __name__)
 
+
 @auth_bp.route("/register", methods=["POST"])
 def register():
     data = request.get_json(force=True, silent=True) or {}
